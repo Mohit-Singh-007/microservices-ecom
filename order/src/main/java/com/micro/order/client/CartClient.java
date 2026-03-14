96c2d11acc4af5def752560db2ae9ed41aa7b974
Mohit <msms789549@gmail.com>
2026-03-14 08:19:03 +0530

    order service | feign-client for product,cart

--- order/src/main/java/com/micro/order/client/CartClient.java
+++ order/src/main/java/com/micro/order/client/CartClient.java
@@ -1,7 +1,18 @@
 package com.micro.order.client;
 
+import com.micro.order.dto.CartRes;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "CART")
 public interface CartClient {
+
+    @GetMapping("/api/cart")
+    CartRes getCart(@RequestHeader("Authorization")String token);
+
+    @DeleteMapping("/api/cart")
+    void clearCart(@RequestHeader("Authorization")String token);
+
 }
---
+++ order/src/main/java/com/micro/order/dto/CartRes.java
@@ -0,0 +1,7 @@
+package com.micro.order.dto;
+
+import lombok.Data;
+
+@Data
+public class CartRes {
+}
